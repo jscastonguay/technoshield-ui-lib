dbcd0146cc0cfe5532581b1d88a8c10d236d7bc1
Jean-Sébastien Castonguay <jscastonguay@gmail.com>
2017-05-07 22:11:23 -0400

    During dev.

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Technoshield-ui.h>
 
+Technoshield_ui ui(0,0);
+
+void setup() {
+  ui.begin();
+  ui.print("hello, world!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+   ui.setCursor(0, 1);
+  // print the number of seconds since reset:
+  ui.print(millis()/1000);
+
+  ui.setBacklight(HIGH);
+  delay(500);
+  ui.setBacklight(LOW);
+  delay(500);
 
 }
